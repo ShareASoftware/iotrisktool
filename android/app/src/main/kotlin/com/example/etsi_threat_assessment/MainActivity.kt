a31847256e6bc3d72e441c70aa8875704b63f5b5
ShareASoftware <girardi.maurizio83@gmail.com>
2025-05-11 10:25:46 +0200

    Replaced with a web based flutter project, removed multiplatform dependencies

--- android/app/src/main/kotlin/com/example/etsi_threat_assessment/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.etsi_threat_assessment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:etsi_threat_assessment/main.dart';
+import 'package:iotrisktool/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
